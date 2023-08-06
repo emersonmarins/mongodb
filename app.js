a564a33e2d36fac9f9fdd88f5351f7fbf38447fd
Emerson Marins <emersonmarinscoutinho@gmail.com>
2023-08-06 17:07:33 -0300

    new pipeline created with  and  methods

--- app.js
+++ app.js
@@ -21,11 +21,32 @@ const pipeline = [
     },
 ]
 
+const pipeline2 = [
+    {
+        $match: {
+            account_type: "checking",
+            balance: { $gt: 100 }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            account_id: 1,
+            account_type: 1,
+            balance: 1,
+            // GBP stands for Great British Pound
+            gbp_balance: { $divide: [ "$balance", 1.3 ] },
+        }
+    }
+]
+
 const main = async () => {
     try {
         await client.connect();
         console.log(`Connected to the database 🌍. \nFull connection string: ${uri}`)
-        let result = await accountsCollection.aggregate(pipeline);
+        // let result = await accountsCollection.aggregate(pipeline);
+        let result = await accountsCollection.aggregate(pipeline2);
+
         for await (const doc of result) {
             console.log(doc);
         }
